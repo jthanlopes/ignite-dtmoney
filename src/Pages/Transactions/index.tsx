de45bcc28c2dc21657a0ccc323adeb25048d2b56
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-17 18:47:54 -0300

    feat: implementação do componente SearchForm

--- src/Pages/Transactions/index.tsx
+++ src/Pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 
 import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
 
@@ -10,6 +11,8 @@ export function Transactions() {
       <Summary />
 
       <TransactionContainer>
+        <SearchForm />
+
         <TransactionTable>
           <tbody>
             <tr>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,8 +9,7 @@ export const GlobalStyles = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2p
-    x ${props => props.theme['green-500']}
+    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
   }
 
   body {
