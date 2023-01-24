f2bbd5a6e7a4fb1b2a02efcc052a7be4443bef8f
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-24 11:20:28 -0300

    feat: buscando transações

--- src/Pages/Transactions/components/SearchForm/index.tsx
+++ src/Pages/Transactions/components/SearchForm/index.tsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { MagnifyingGlass } from "phosphor-react";
 import { useForm } from "react-hook-form";
-import { SearchFormContainer } from "./styles";
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
+
+import { SearchFormContainer } from "./styles";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -11,6 +15,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
+
   const {
     register,
     handleSubmit,
@@ -19,8 +25,8 @@ export function SearchForm() {
     resolver: zodResolver(searchFormSchema),
   });
 
-  function handleSearchForm(data: SearchFormInputs) {
-    console.log(data);
+  async function handleSearchForm(data: SearchFormInputs) {
+    await fetchTransactions(data.query);
   }
 
   return (
